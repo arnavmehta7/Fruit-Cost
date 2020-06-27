44082f0ee1dbb0f7f9ac69a824172059331be858
arnavmehta7 <65492948+arnavmehta7@users.noreply.github.com>
2020-06-27 14:04:53 +0530

    Create fruit_cost.py

---
+++ fruit_cost.py
@@ -0,0 +1,34 @@
+name = input("What is your name?:-")
+age=input("What is your age?:-")
+
+print("So you are "+name+" and your age is "+age)
+
+
+print("You are very hardworking.")
+else:print("HELLO CARBON PARTICLE")
+
+permis=input("Do you want fruits:-")
+if permis=="yes" or permis=="Yes":
+ fruit=input("Which fruit do you want:-")
+print("Okay you want "+fruit)
+quant=input
+
+
+("How much quantity for "+fruit+"(in kg):-")
+
+order="You want {0} {1}"
+
+money="$"
+import random
+print(order.format(quant,fruit)+str(".So price will be")+" "+str(random.randint(5,20))+"$")
+
+
+
+
+
+
+
+
+
+
+
